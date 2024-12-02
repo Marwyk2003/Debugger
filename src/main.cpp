79a075ec20bcae3445e0b07d55f35bcd9e4eef8b
Marwyk2003 <marwyk2003@gmail.com>
2024-12-02 21:02:15 +0100

    Generate multiple files

--- src/main.cpp
+++ src/main.cpp
@@ -7,6 +7,7 @@
 #include <fstream>
 #include <string>
 #include <iomanip>
+#include <map>
 
 using namespace std;
 
@@ -18,15 +19,14 @@ using namespace std;
 #define END_OUT "E_OUT"
 
 string program_name;
-ofstream result;
 
-void writePackage(char*,bool);
+void writePackage(char *, bool);
+void writeHeader(ofstream &, string);
+
+map<string, ofstream> pids;
 
 int rootProcess()
 {
-    result.open("result.html", ios::out | ios::app);
-    // read input, create html etc.
-
     // SELECT
     fd_set read_fds;
     int select_fd = 5;
@@ -66,7 +66,6 @@ int rootProcess()
                 writePackage(buf, true);
         }
     }
-    result.close();
     return 0;
 }
 
@@ -119,7 +118,7 @@ int childProcess(char *program, char *argv[])
             auto now = chrono::system_clock::now();
             auto currentTime = chrono::duration_cast<chrono::milliseconds>(now.time_since_epoch()).count();
 
-            string pref = BEGIN_OUT + string("\n") + to_string(currentTime) + "\n", suf = END_OUT + string("\n");
+            string pref = BEGIN_OUT + string("\n") + to_string(getpid()) + "\n" + to_string(currentTime) + "\n", suf = END_OUT + string("\n");
 
             rbytes = read(FD_OUT, buf + pref.size(), BUF_SIZE);
             if (rbytes <= 0)
@@ -137,7 +136,7 @@ int childProcess(char *program, char *argv[])
             auto now = chrono::system_clock::now();
             auto currentTime = chrono::duration_cast<chrono::milliseconds>(now.time_since_epoch()).count();
 
-            string pref = BEGIN_OUT + string("\n") + to_string(currentTime) + "\n", suf = END_OUT + string("\n");
+            string pref = BEGIN_OUT + string("\n") + to_string(getpid()) + "\n" + to_string(currentTime) + "\n", suf = END_OUT + string("\n");
 
             rbytes = read(FD_ERR, buf + pref.size(), BUF_SIZE);
             if (rbytes <= 0)
@@ -150,11 +149,11 @@ int childProcess(char *program, char *argv[])
             }
         }
     }
-    result.close();
     return 0;
 }
 
-void writeHeader(string program_name){
+void writeHeader(ofstream &result, string program_name)
+{
     auto now = chrono::system_clock::now();
     auto time = chrono::system_clock::to_time_t(now);
     result << R"(<!DOCTYPE html>
@@ -169,12 +168,13 @@ void writeHeader(string program_name){
 <body>
 <div class="head">
 <div class="info">
-<span class="info-title">command:</span> <span class="info-value info-value-path">)" + program_name + R"(</span>
+<span class="info-title">command:</span> <span class="info-value info-value-path">)" +
+                  program_name + R"(</span>
 </div>
 <div class="info">
 <span class="info-title">start time:</span> <span class="info-value">)";
-result << put_time(localtime(&time), "%Y-%m-%d %H:%M:%S");
-result << R"(</span>
+    result << put_time(localtime(&time), "%Y-%m-%d %H:%M:%S");
+    result << R"(</span>
 </div>
 <div class="info">
 <span class="info-title">last entry:</span> <span class="info-value"></span>
@@ -188,15 +188,16 @@ result << R"(</span>
     result.flush();
 }
 
-void writeLine(string line, string timeStr, bool isError){
+void writeLine(ofstream &result, string line, string timeStr, bool isError)
+{
     long long milliseconds = stoll(timeStr);
     auto duration = chrono::milliseconds(milliseconds);
     auto time_point = chrono::system_clock::time_point(duration);
-    time_t time= chrono::system_clock::to_time_t(time_point);
+    time_t time = chrono::system_clock::to_time_t(time_point);
 
     result << "<tr><td class=\"entry-time\">";
     result << put_time(localtime(&time), "%Y-%m-%d %H:%M:%S");
-    if(isError)
+    if (isError)
         result << "</td><td>&nbsp;</td><td class=\"entry-log entry-stderr\">";
     else
         result << "</td><td>&nbsp;</td><td class=\"entry-log entry-stdout\">";
@@ -205,33 +206,41 @@ void writeLine(string line, string timeStr, bool isError){
     result.flush();
 }
 
-void writePackage(char * buf, bool isError){
+void writePackage(char *buf, bool isError)
+{
     istringstream stream(buf);
     string line;
-    string time;
+    string pid, time;
 
-    while (getline(stream, line)) {
-        if(line == BEGIN_OUT){
+    while (getline(stream, line))
+    {
+        if (line[0] == 'B')
+        {
+            getline(stream, pid);
             getline(stream, time);
             continue;
         }
-        if(line == END_OUT){
+        if (line[0] == 'E')
+        {
             continue;
         }
-        writeLine(line, time, isError);
+
+        if (pids.find(pid) == pids.end())
+        {
+            pids[pid].open("result_" + pid + ".html", ios::out | ios::trunc);
+            writeHeader(pids[pid], "");
+        }
+        ofstream &s = pids[pid];
+        writeLine(s, line, time, isError);
     }
 }
 
 int main(int, char *argv[])
 {
-
-    result.open("result.html", ios::out | ios::trunc);
-    writeHeader(argv[1]);
-    result.close();
     char *env_var = getenv(ENV_NAME);
     if (!env_var)
     {
-        putenv(argv[0]);
+        setenv(ENV_NAME, argv[0], 1);
 
         // create pipe ROOT <- LISTENER 1
         int pipe_fd_out[2], pipe_fd_err[2];
