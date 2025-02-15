05aa3d8da628fff21fb4aa36bff4b9adc79267d0
Adam Szwaja <szwajaadam@gmail.com>
2025-02-15 21:00:41 +0100

    revert performance changes to fix nasty crash

--- src/fds_listner.cpp
+++ src/fds_listner.cpp
@@ -24,19 +24,22 @@ static bool read_to_buffer(int fd, char* buf, int& buf_size, bool ended) {
 static void process_buffer(char* buf, int& buf_size, bool& ended, std::function<void(int, char*)> process) {
     if (buf_size == 0) return;
     unsigned int header_key;
-    int start;
+    int start, i;
     while (true) {
         memcpy(&header_key, buf, sizeof(int));
         start = (header_key == HEADER_CONST) ? sizeof(package_header) : 0;
-        auto it = std::find(buf + start, buf + start + buf_size, '\n');
-        if (it == buf + start + buf_size) {
+        for (i = start; i < buf_size; i++) {
+            if (buf[i] == '\n') {
+                break;
+            }
+        }
+        if (i >= buf_size) {
             if (ended) {
                 process(buf_size, buf);
                 buf_size = 0;
             }
             break;
         }
-        int i = std::distance(buf, it);
         process(i + 1, buf);
         memmove(buf, buf + i + 1, (buf_size - i - 1) * sizeof(char));
         buf_size -= i + 1;
